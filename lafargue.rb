e5414b7e634fa0bb036fea072f88657dd9c06873
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-01-22 11:58:21 -0300

    Primera parte del bot

---
+++ lafargue.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+# Paul Lafargue (Santiago de Cuba, 15 de enero de 1842 - Draveil, 26 de
+# noviembre de 1911) fue un periodista, médico, teórico político y
+# revolucionario francocubano. Aunque en un principio su actividad
+# política se orientó a partir de la obra de Proudhon, el contacto con
+# Karl Marx (del que llegó a ser yerno al casarse con su segunda hija,
+# Laura) acabó siendo determinante. Su obra más conocida es El Derecho a
+# la Pereza.  http://es.wikipedia.org/wiki/Paul_Lafargue
+require 'cinch'
+require 'zmqmachine'
+
