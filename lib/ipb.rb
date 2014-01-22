93a7c84841fcabb3d135fbb2ec9eb680818407c3
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-01-22 15:02:24 -0300

    Listar mejor /names y /list

--- lib/ipb.rb
+++ lib/ipb.rb
@@ -11,6 +11,30 @@ class IPB
 
   # Cuando llega un mensaje a un canal
   def message(m)
+
+    return if m.action?
+
+    # Si estamos preguntando por los canales devolver una lista y dejar
+    # de procesar
+    if not m.message.match(/\/list/).nil?
+      $networks.each do |n|
+        m.reply "#{n[:server]} => #{n[:bot].channels.map { |c| c.name }.to_s.gsub(/["\[\]]/, '') }"
+      end
+
+      return
+    end
+
+    # Devuelve la lista de nicks de cada canal y red
+    if not m.message.match(/\/names/).nil?
+      $networks.each do |n|
+        n[:bot].channels.each do |c|
+          puts c.users
+          m.reply "#{n[:server]}/#{c.name} (#{c.users.count}): #{c.users.keys.map { |u| u.nick }.to_s.gsub(/["\[\]]/, '')}"
+        end
+      end
+      return
+    end
+
     # Por cada red
     $networks.each do |n|
       # Excepto en la presente
@@ -19,7 +43,7 @@ class IPB
       # Y en cada canal
       n[:bot].channels.each do |c|
         # Repetir lo que dijeron
-        c.send "#{m.user}: \"#{m.message}\""
+        c.send "#{m.user}: #{m.message}"
       end
     end
   end
@@ -30,7 +54,7 @@ class IPB
       next if n[:server] == m.bot.config.server
 
       n[:bot].channels.each do |c|
-        c.send "* #{m.user} #{m.message}"
+        c.send "* #{m.user} #{m.message.gsub(/ACTION /, '')}"
       end
     end
   end
